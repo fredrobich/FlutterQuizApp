a8a85749ba6f432699ea88022354ff224a5e1aec
Patrick-Tresor <yaopatrick6@gmail.com>
2019-10-31 16:43:48 -0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Quiz App"),
+        title: Text("Quiz"),
         elevation: 0.0,
       ),
       body: RefreshIndicator(
